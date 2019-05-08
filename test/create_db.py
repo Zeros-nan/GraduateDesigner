10b9e93476fb2263477b7cbcb147d316928bea0b
nanmu <eenan.wong@gmail.com>
2019-05-08 20:46:50 +0800

    learn facelite in test

--- test/create_db.py
+++ test/create_db.py
@@ -1,6 +1,6 @@
 from models import db, User
 db.create_all()
-user = User(username='nanmu', email='123@126.com')
+user = User(username='nanmu', email='123@233.com')
 user.set_password('233')
 db.session.add(user)
 db.session.commit()
